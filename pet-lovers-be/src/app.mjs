844479492d93641627b2a50c14862db493e38b0f
Rudi <rudolf.gumzej@biss.hr>
2023-05-08 13:07:07 +0200

    updated app.mjs and vercel.json

--- pet-lovers-be/src/app.mjs
+++ pet-lovers-be/src/app.mjs
@@ -8,6 +8,11 @@ const __filename = fileURLToPath(import.meta.url);
 
 const __dirname = path.dirname(__filename);
 const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+  console.log(`API listening on PORT ${PORT} `);
+});
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "..", "public")));
 app.use(
